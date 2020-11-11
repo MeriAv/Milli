3a364b22eef8faa11e187f4ad37af86351e43274
MeriAv <72850780+MeriAv@users.noreply.github.com>
2020-11-11 11:45:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,6 @@ public class Main {
         int ticket = 25000;
         int bonus = 20;
         int result = ticket / bonus;
-        int score = 0;
-        if (result >= 1) {
-            score = result;
-        }
-        System.out.println (score);
+        System.out.println (result);
     }
 }
